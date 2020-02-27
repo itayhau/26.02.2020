f30204c7a0675c6df91c630b03eea3ecb272df00
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-02-27 10:17:46 +0200

    Add files via upload

---
+++ FlyWeight_cs/Flyweight/BigNumberCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    public static class BigNumberCreator
+    {
+        static Dictionary<int, BigInteger> repository = new Dictionary<int, BigInteger>();
+
+        public static BigInteger FetchNumber(int key)
+        {
+            // look ...
+            if (repository.TryGetValue(key, out BigInteger big))
+            {
+                return big;
+            }
+
+            // create and store
+            BigInteger biggy = (BigInteger)Math.Pow(2, key);
+            repository[key] = biggy;
+
+            return biggy;
+
+        }
+
+    }
+}
---
+++ FlyWeight_cs/Flyweight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2^40, 2^80, 2^100
+            long l = (long)Math.Pow(2, 40);
+            int[] fly_weight = {  40, 80, 100, 200, 400 }; // ...........................
+
+            PrintSequence(fly_weight);
+
+            Console.WriteLine(l);
+        }
+
+        static void PrintSequence(int[] flyweight)
+        {
+            // actually need the big objects
+            foreach (var item in flyweight)
+            {
+              Console.WriteLine(BigNumberCreator.FetchNumber(item));
+            }
+        }
+    }
+}
